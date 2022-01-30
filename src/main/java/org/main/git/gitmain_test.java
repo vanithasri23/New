7bef92e4341d85f34753d7314c65675ea6ce81ec
vanithasri23 <vanithasriselvaraj23@gmail.com>
2022-01-30 12:54:26 +0530

    git new test

---
+++ src/main/java/org/main/git/gitmain_test.java
@@ -0,0 +1,10 @@
+package org.main.git;
+
+public class gitmain_test {
+
+	public static void main(String[] args) {
+		System.out.println("welcome main");
+
+	}
+
+}
---
+++ src/test/java/org/git/test/Git_test.java
@@ -0,0 +1,9 @@
+package org.git.test;
+
+public class Git_test {
+
+	public static void main(String[] args) {
+System.out.println("welcome test");
+	}
+
+}
