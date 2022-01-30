0cab57592487c567991bf2c9d91c154e01266dcc
vanithasri23 <vanithasriselvaraj23@gmail.com>
2022-01-30 13:53:38 +0530

    sri code

--- src/test/java/org/git/test/Git_test.java
+++ src/test/java/org/git/test/Git_test.java
@@ -3,7 +3,11 @@ package org.git.test;
 public class Git_test {
 
 	public static void main(String[] args) {
-System.out.println("welcome test");
+		System.out.println("welcome test");
+		System.out.println("welcome test day1");
+		System.out.println("welcome test sri");
+
+
 	}
 
 }
